6a96719a9d368458c5823deec998c5464d179220
baraluga <baperaltaa@gmail.com>
2019-10-06 13:36:35 +0800

    Frontend: Finalized getTopics implementation!

--- frontend/react-js/src/components/topics/Topics.jsx
+++ frontend/react-js/src/components/topics/Topics.jsx
@@ -4,17 +4,41 @@ import { endpoints } from '../../utils';
 
 export const Topics = ({ token }) => {
   const [topics, setTopics] = useState([]);
-  const [loading, setLoading] = useState(loading);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    let ignore = false;
     setLoading(true);
     setError('');
-    Axios.get(endpoints.topics.getTopics)
-      .then(({ data }) => setTopics(data.data))
-      .catch(({ response }) => setError(response.data.message))
-      .finally(_ => setLoading(false));
+    Axios.get(endpoints.topics.getTopics, {
+      headers: { token: token },
+    })
+      .then(({ data }) => (!ignore ? setTopics(data.data) : null))
+      .catch(({ response }) =>
+        !ignore ? setError(response.data.message) : null,
+      )
+      .finally(_ => (!ignore ? setLoading(false) : null));
+    return () => (ignore = true);
+    // eslint-disable-next-line
   }, []);
 
-  return <div>topics page</div>;
+  return (
+    <div className="topic-container">
+      <h2>Your Topics</h2>
+      {loading && <div className="loading">Loading topics...</div>}
+      {error && <div className="error">{error}</div>}
+      {(topics || []).map(topic => renderTopic(topic))}
+    </div>
+  );
 };
+
+// render functions;
+
+const renderTopic = ({ subject, description, updatedAt }) => (
+  <div className="topic">
+    <h4>{subject}</h4>
+    <h5>{description}</h5>
+    Last updated at: {updatedAt}
+  </div>
+);
